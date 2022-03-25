1b9af6ec3e54b8f8e98635872bbeba3f91b83dc0
Juan Marco <jmmarco@gmail.com>
2022-03-25 11:50:32 +0100

    Add default APP values and housekeeping

--- index.mjs
+++ index.mjs
@@ -1,33 +1,28 @@
-import {
-  populateGrid,
-  generateGrid,
-  generateStepper,
-  handleStepChange,
-} from "./helpers.mjs";
+import { populateGrid, generateGrid, generateStepper } from "./helpers.mjs";
 
 (() => {
-
   const APP_SETTINGS = {
-    maxGridSize: 600,
-    defaultCellSize: 47,
-  }
-  const myGrid = generateGrid(600);
-  populateGrid(37, myGrid);
+    defaultGridDimensions: 600,
+    defaultGridSize: 47,
+  };
+
+  const myGrid = generateGrid(APP_SETTINGS.defaultGridDimensions);
+  populateGrid(64, myGrid);
 
   const gridContainer = document.querySelector(".grid-container");
   const gridControls = document.querySelector(".grid-controls");
   gridContainer.append(myGrid);
-  gridControls.append(generateStepper(1, 60));
+  gridControls.append(generateStepper(1, 64));
 
   const stepper = document.getElementById("gridSize");
   stepper.addEventListener("input", (e) => {
-  
-    document.getElementById('gridSizeValue').textContent = e.target.value;
-    // const span = document.getElementById('gridSizeValue')
-    // console.log(span)
-    gridContainer.replaceChildren()
-    const myGrid = generateGrid(600);
-    populateGrid(e.target.value, myGrid);
+    const { value } = e.target;
+    document.getElementById(
+      "gridSizeValue"
+    ).textContent = `${value} x ${value}`;
+    gridContainer.replaceChildren();
+    const myGrid = generateGrid(APP_SETTINGS.defaultGridSize);
+    populateGrid(value, myGrid);
     gridContainer.append(myGrid);
   });
 })();
